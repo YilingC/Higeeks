5f8b60e847041b5e1b19e1b5ec1418f2e9d035f3
Yiling <ylsisfor@gmail.com>
2016-08-04 08:49:16 +0800

    update

--- app/src/main/java/com/example/higeeks/higeeks/ActivityEm.java
+++ app/src/main/java/com/example/higeeks/higeeks/ActivityEm.java
@@ -1,18 +1,52 @@
 package com.example.higeeks.higeeks;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ActivityEm extends AppCompatActivity {
+public class ActivityEm extends Activity implements View.OnClickListener {
+
+    String EXTRA_MESSAGE = "extra_message";
+    String message = "This is a message sent from Em !!!";
+    Button btnToYi;
+    TextView textMsgFromYi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_em);
+        initLayoutComponent();
+        String msgReceived = getIntent().getStringExtra(EXTRA_MESSAGE);
+        if (!TextUtils.isEmpty(msgReceived)) {
+            textMsgFromYi.setText(msgReceived);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnToEm:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void initLayoutComponent() {
+
+        btnToYi = (Button) findViewById(R.id.btnToYi);
+        textMsgFromYi = (TextView) findViewById(R.id.textMsgFromYi);
     }
 
 }
--- app/src/main/java/com/example/higeeks/higeeks/MainActivity.java
+++ app/src/main/java/com/example/higeeks/higeeks/MainActivity.java
@@ -3,18 +3,27 @@ package com.example.higeeks.higeeks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
+    String EXTRA_MESSAGE = "extra_message";
+    String message = "This is a message sent from Yi !!!";
     Button btnToEm;
+    TextView textMsgFromEm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initLayoutComponent();
+        String msgReceived = getIntent().getStringExtra(EXTRA_MESSAGE);
+        if (!TextUtils.isEmpty(msgReceived)) {
+            textMsgFromEm.setText(msgReceived);
+        }
     }
 
     @Override
@@ -22,6 +31,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         switch (view.getId()) {
             case R.id.btnToEm:
                 Intent intent = new Intent(this, ActivityEm.class);
+                intent.putExtra(EXTRA_MESSAGE, message);
                 startActivity(intent);
                 break;
             default:
@@ -30,6 +40,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
     public void initLayoutComponent() {
+
         btnToEm = (Button) findViewById(R.id.btnToEm);
+        textMsgFromEm = (TextView) findViewById(R.id.textMsgFromEm);
     }
 }
